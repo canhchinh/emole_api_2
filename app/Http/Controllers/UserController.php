76537b75325971008bc7a5fa968227744f2c1b18
tonyStack101 <tonyGithub@gmail.com>
2021-02-06 09:48:53 +0700

    user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,8 +6,48 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\CreateUser;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 class UserController extends Controller
 {
+    public function login(Request $request)
+    {
+        try {
+            $request->validate([
+                'email' => 'email|required',
+                'password' => 'required'
+            ]);
+
+            $credentials = request(['email', 'password']);
+
+            if (!Auth::attempt($credentials)) {
+                return response()->json([
+                    'status_code' => 500,
+                    'message' => 'Unauthorized'
+                ]);
+            }
+            $user = User::where('email', $request->email)->first();
+
+            if (!Hash::check($request->password, $user->password, [])) {
+                throw new \Exception('Error in Login');
+            }
+
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+            return response()->json([
+                'status_code' => 200,
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status_code' => 500,
+                'message' => 'Error in Login',
+                'error' => $e->getMessage(),
+            ]);
+        }
+    }
+
+
     public function register(CreateUser $request)
     {
         $req = $request->all();
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
@@ -21,7 +20,7 @@ use App\Http\Controllers\UserController;
 
 Route::group(['prefix' => 'user'], function() {
     Route::post('register', [UserController::class, 'register']);
-    Route::post('login', [AuthController::class, 'login']);
+    Route::post('login', [UserController::class, 'login']);
 });
 
 Route::middleware(['auth:sanctum'])->group(function () {
