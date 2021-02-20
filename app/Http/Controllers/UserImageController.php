7c20ed810db47a58559f52f83202b756b9fdba9d
tonyStack101 <tonyGithub@gmail.com>
2021-02-20 17:25:53 +0700

    upload image

--- app/Http/Controllers/UserImageController.php
+++ app/Http/Controllers/UserImageController.php
@@ -5,24 +5,25 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use App\Models\UserImage;
+use Illuminate\Support\Facades\File;
 class UserImageController extends Controller
 {
     public function newImageUpload(Request $request)
     {
         try {
-            $req = $request->all();
             $user = $request->user();
-            $base64_str = substr($req['image'], strpos($req['image'], ",")+1);
-            //decode base64 string
-            $image = base64_decode($base64_str);
-            $suffix = $user->id.'/'.time().'.png';
-            $path = 'public/'.$suffix;
-            Storage::disk('local')->put($path, $image);
-            $link = '/storage/'.$suffix;
-            UserImage::create([
-                'user_id' => $user->id,
-                'url' => $link
-            ]);
+            $file = $request->file('image');
+            $extension = $file->getClientOriginalExtension();
+            if(in_array($extension, ['jpg', 'png', 'jpeg'])) {
+                $path = 'user/' . $user->id . '/'. time() . '.' . $extension;
+                Storage::disk('public')->put($path,  File::get($file));
+                $url = '/storage/'.$path;
+
+                UserImage::create([
+                    'user_id' => $user->id,
+                    'url' => $url
+                ]);
+            }
             return response()->json([
                 'status' => true
             ]);
