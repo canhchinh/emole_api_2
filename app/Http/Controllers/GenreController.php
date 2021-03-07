4a1560bba593aee9102ceff419cc03b20d5f1486
tonyStack101 <tonyGithub@gmail.com>
2021-03-07 15:51:58 +0700

    api

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -13,6 +13,31 @@ class GenreController extends Controller
     ) {
         $this->genreRepo = $genreRepo;
     }
+    /**
+     * @OA\Get(
+     *   path="/career/{career_id}/genre/list",
+     *   summary="genre sns",
+     *   operationId="list_genre",
+     *   tags={"Genre"},
+     *   security={ {"token": {}} },
+     *     @OA\Parameter(
+     *         description="ID of career",
+     *         in="path",
+     *         name="career_id",
+     *         required=true,
+     *         @OA\Schema(
+     *           type="integer",
+     *           format="int64"
+     *         )
+     *     ),
+     *   @OA\Response(response=200, description="successful operation", @OA\JsonContent()),
+     *   @OA\Response(response=400, description="Bad request", @OA\JsonContent()),
+     *   @OA\Response(response=401, description="Unauthorized", @OA\JsonContent()),
+     *   @OA\Response(response=403, description="Forbidden", @OA\JsonContent()),
+     *   @OA\Response(response=404, description="Resource Not Found", @OA\JsonContent()),
+     *   @OA\Response(response=500, description="Internal Server Error", @OA\JsonContent()),
+     * )
+     */
     public function listGenre($careerId)
     {
         $genre = $this->genreRepo->where('career_id', $careerId)
