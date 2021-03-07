b20f8ec0ad2ed08d09bd291ea3b6dd8e4c7f332b
tonyStack101 <tonyGithub@gmail.com>
2021-03-07 19:14:39 +0700

    migration

--- database/migrations/2021_02_08_045602_create_user_career_table.php
+++ database/migrations/2021_02_08_045602_create_user_career_table.php
@@ -18,9 +18,9 @@ class CreateUserCareerTable extends Migration
             $table->integer('user_id')->nullable();
             $table->integer('career_id')->nullable();
             $table->text('tag')->nullable();
-            $table->jsonb('category_ids')->nullable();
-            $table->jsonb('job_ids')->nullable();
-            $table->jsonb('genre_ids')->nullable();
+            $table->json('category_ids')->nullable();
+            $table->json('job_ids')->nullable();
+            $table->json('genre_ids')->nullable();
         });
     }
 
