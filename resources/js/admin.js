a8ce2df9c404f26d225f67918d7dac90aeab4224
Toan Nguyen <toan.nguyen@ebuynow.com>
2021-03-26 17:55:52 +0700

    Add multi select libs

---
+++ resources/js/admin.js
@@ -0,0 +1,44 @@
+$(function () {
+    Admin.init();
+});
+
+var Admin = function () {
+    return {
+        init: function () {
+            this.registerLinkWithDeleteMethod();
+
+            var els = $('.selectizeSelect');
+            els?.each(function () {
+                $(this).selectize({
+                    plugins: ['remove_button'],
+                    persist: true,
+                    maxItems: null,
+                    valueField: 'id',
+                    labelField: 'title',
+                    searchField: 'title',
+                    options: $(this).data('json'),
+                    create: true
+                });
+            });
+        },
+        registerLinkWithDeleteMethod: function () {
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            $(document).on('click', 'a.js-click', function (e) {
+                e.preventDefault();
+                var $this = $(this);
+                $.post({
+                    type: $this.data('method'),
+                    url: $this.attr('href')
+                }).done(function (data) {
+                    if (data.hasOwnProperty('url')) {
+                        window.location.href = data.url;
+                    }
+                });
+            });
+        }
+    }
+}();
