9d0638654e2b22ad77dbc878715d76c495706e03
Chinh Phan <phancanhchinh@gmail.com>
2021-04-22 22:26:20 +0700

    update

---
+++ database/migrations/2021_04_22_150018_add_column_temp_tiktok.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnTempTiktok extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->json("temp_tiktok_user")->nullable()->after("tiktok_user");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $table->dropColumn("temp_tiktok_user");
+    }
+}
