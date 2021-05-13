0d292fc05049b1246f747aa3423dda1760807cdf
BachPhan <phanxuanbachkh@gmail.com>
2021-05-13 09:50:43 +0700

    b

--- resources/views/api/mail_register.blade.php
+++ resources/views/api/mail_register.blade.php
@@ -1,14 +1,7 @@
 
 
 @component('mail::layout')
-{{-- Header --}}
-@slot ('header')
-{{-- @component('mail::header', ['url' => config('app.url')]) --}}
-    <!-- header -->
-{{-- @endcomponent --}}
-@endslot
 
-{{-- Content here --}}
 @component('mail::message')
 <p>この度は、emoleへ登録していただき、 ありがとうございます。</p>
 <p>以下のボタンからメールアドレスを認証して 登録を完了してください。ボタン：</p>
@@ -17,17 +10,4 @@
 @endcomponent
 @endcomponent
 
-{{-- Subcopy --}}
-@slot('subcopy')
-{{-- @component('mail::subcopy') --}}
-<!-- subcopy -->
-{{-- @endcomponent --}}
-@endslot
-
-{{-- Footer --}}
-@slot ('footer')
-{{-- @component('mail::footer') --}}
-<!-- footer -->
-{{-- @endcomponent --}}
-@endslot
 @endcomponent
