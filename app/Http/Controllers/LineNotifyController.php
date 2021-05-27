f9f63cd6c5b234a1266fb97c39de35704a329dcb
Chinh Phan <phancanhchinh@gmail.com>
2021-05-27 21:12:27 +0700

    update

--- app/Http/Controllers/LineNotifyController.php
+++ app/Http/Controllers/LineNotifyController.php
@@ -126,7 +126,7 @@ class LineNotifyController extends Controller
         ]);
 
         try {
-            $message = (new LaravelLineMessage());
+            $message = (new LaravelLineMessage())->message($request->message)->http(["google" => $request->url]);
             return response()->json([
                 'status' => $message,
             ]);
