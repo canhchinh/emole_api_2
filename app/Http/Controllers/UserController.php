b5da59c1e08a298963703c5b8d7ba675b3cf2a08
Chinh Phan <phancanhchinh@gmail.com>
2021-05-28 00:51:08 +0700

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -872,7 +872,7 @@ class UserController extends Controller
             if(!empty($members)) {
                 $memberIds = [];
                 foreach($members as $member) {
-                    if(!empty($member['id'])) {
+                    if($member['id']) {
                         array_push($memberIds, $member['id']);
                     }
                 }
