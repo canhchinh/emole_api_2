7fc714c2ec41523eaedd8159bafc9d9832c4b7b7
Chinh Phan <phancanhchinh@gmail.com>
2021-06-09 19:08:02 +0700

    update

--- app/Repositories/UserRepositoryEloquent.php
+++ app/Repositories/UserRepositoryEloquent.php
@@ -86,7 +86,7 @@ class UserRepositoryEloquent extends BaseRepository implements UserRepository
         $users = User::select('*')
             ->where('id', '<>', $userId)
             ->where('active', '<>', 0)
-            ->with(['activity_base', 'portfolio'])->orderBy('id', 'DESC')->get();
+            ->with(['activity_base', 'portfolio'])->has('portfolio')->orderBy('id', 'DESC')->get();
         return $users;
         if(!empty($filters['keyword'])) {
             $users->where('user_name', 'like', '%'.$filters['keyword'].'%')
