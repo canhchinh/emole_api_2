10fdd6b544390ec5b24762def3efc29f14d83ce7
Chinh Phan <phancanhchinh@gmail.com>
2021-06-28 23:31:16 +0700

    test

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -60,7 +60,7 @@ class SocialController extends Controller
             $token = $request->get('token');
             $access_token = $this->fbService->getLongTermToken($token);
             $this->fbService->getFacebook()->setDefaultAccessToken($token);
-            $user_info = $this->fbService->getUserInfo('me');
+            $user_info = $this->fbService->getUserInfo('me?fields=accounts');
             return response()->json([
                 'status' => true,
                 'info' => $user_info,
