077deff01e88187c3c2f6913faa80e5f89f36180
Chinh Phan <phancanhchinh@gmail.com>
2021-06-28 23:05:22 +0700

    test

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -61,14 +61,14 @@ class SocialController extends Controller
             $access_token = $this->fbService->getLongTermToken($token);
             $this->fbService->getFacebook()->setDefaultAccessToken($token);
             $user_info = $this->fbService->getUserInfo('me?fields=accounts{connected_instagram_account}');
-            return response()->json([
-                'status' => true,
-                'info' => $user_info,
-            ]);
             if ($user_info && !empty($user_info['accounts'])) {
                 $connected_instagram_account_id = $user_info['accounts']['data'][0]['connected_instagram_account']['id'];
                 // $connected_instagram_account_id = $user_info['id'];
                 $user_info = $this->fbService->getUserInfo("$connected_instagram_account_id?fields=username");
+                return response()->json([
+                    'status' => true,
+                    'info' => $user_info,
+                ]);
                 $user_name = $user_info["username"] ?? null;
             }
             // $user = $this->userRepo->where('facebook_id', $connected_instagram_account_id)->first();
